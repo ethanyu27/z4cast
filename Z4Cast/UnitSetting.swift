1d0d00f8eab19bc0c7497c65707f052ecbfd708a
Ethan Yu <yu920@purdue.edu>
2021-06-05 21:46:31 -1000

    first commit

---
+++ Z4Cast/UnitSetting.swift
@@ -0,0 +1,9 @@
+//
+//  UnitSetting.swift
+//  Z4Cast
+//
+//  Created by Ethan Yu on 6/5/21.
+//  Copyright © 2021 Yutopia Productions. All rights reserved.
+//
+
+import Foundation
---
+++ Z4Cast/Weather.swift
@@ -0,0 +1,9 @@
+//
+//  Weather.swift
+//  Z4Cast
+//
+//  Created by Ethan Yu on 6/3/21.
+//  Copyright © 2021 Yutopia Productions. All rights reserved.
+//
+
+import Foundation
